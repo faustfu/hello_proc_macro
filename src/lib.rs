31662aa02e094d4dbbece95a9add21aa65bd2a6c
Faust Fu <faust.fu@gmail.com>
2020-06-10 12:10:19 +0800

    Dev: add custom attribute case.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use self::proc_macro::TokenStream;
 
-#[proc_macro_derive(A)] // declare derive:A
+#[proc_macro_derive(A)] // declare derive(A)
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = input.to_string();
     assert!(input.contains("struct A"));
@@ -16,4 +16,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     "#
     .parse()
     .unwrap()
+}
+
+#[proc_macro_attribute]
+pub fn attr_with_args(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = args.to_string();
+    let input = input.to_string();
+    assert!(input.contains("fn foo()"));
+
+    format!("fn foo() -> &'static str {{ {} }}", args)
+        .parse()
+        .unwrap()
 }
